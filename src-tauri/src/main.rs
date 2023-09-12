0b60d287968e714bbeb39c84965e349d203d08bf
kotobukid <kotobuki@daqua.jp>
2023-09-12 18:28:41 +0900

    受信した文字列をテキストに追記する

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,38 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use std::error::Error;
+use std::fmt::format;
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/comman
+use std::fs::{File, OpenOptions};
+use std::io::{self, Read, Write, BufReader, Seek, SeekFrom};
+
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+    let file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open("../logs/message_history.txt");
+    let name = format!("{}\n", name);
 
+    match file {
+        Ok(mut f) => {
+            let mut buf = BufReader::new(name.as_bytes());
+            let mut bytes = Vec::new();
+            buf.read_to_end(&mut bytes).unwrap();
+
+            f.write_all(&bytes).expect("write failed");
+
+            format!("Hello, {}! You've been greeted from Rust!", name)
+        },
+        Err(e) => {
+            format!("Hello, {}! You've been greeted from Rust!", name)
+            // 失敗した理由も出力するにゃ
+            // Err(e)
+        }
+    }
+}
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
